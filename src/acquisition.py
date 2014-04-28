559627ff69836db4999161d058d8de65351438d6
Norman Saez <norman.saez@gmail.com>
2014-04-28 16:28:12 +0100

    Change to make plots

--- src/acquisition.py
+++ src/acquisition.py
@@ -31,36 +31,44 @@ pxly =d.Get("npxly")[0]
 #'ProgFrameTimeEnable=true;ProgFrameTimeAbs=50000;ExposureTimeAbs=58;'
 
 img_to_take =  options.nimg
-d.Set("aravisGet","?0:ExposureTimeAbs")
-try:
+#d.Set("aravisGet","?0:ExposureTimeAbs")
+#try:
+#    exptime=int(d.Get("aravisGet"))
+#except:
+#    print "Can't get exptime"
+#    exptime=-1
+#print "Current ExposureTimeAbs =%d" % exptime
+#print "Setting to %d in both cameras" % options.exptime
+times_x = [3000,5000,10000,12000,15000]
+fps_e = []
+for exptime in times_x:
+    d.Set("aravisCmdAll",'ExposureTimeAbs=%d;'% exptime)
+    d.Set("aravisGet","?0:ExposureTimeAbs")
     exptime=int(d.Get("aravisGet"))
-except:
-    print "Can't get exptime"
-    exptime=-1
-print "Current ExposureTimeAbs =%d" % exptime
-print "Setting to %d in both cameras" % options.exptime
-d.Set("aravisCmdAll",'ExposureTimeAbs=%d;'% options.exptime)
-d.Set("aravisGet","?0:ExposureTimeAbs")
-exptime=int(d.Get("aravisGet"))
-print "New Exposure Time %d" % exptime
-
-t0 = time.clock()
-for i in range(0, img_to_take+1):
-    #Preparing name for fits:
-    fitsname = "img_exp%s.fits" % str(exptime).zfill(2)
-    print "Taking %d ...." % i
-    #Getting image from buffer
-    stream=d.GetStream('%srtcPxlBuf'%options.prefix)#a single frame
-    #change the default shape of image (default shape is usually: (x*y,1))
-    data = stream[0].reshape(2*pxlx,pxly)
-#    data = stream[0].reshape(pxlx,pxly)
-    #writes a fits
-    FITS.Write(data, fitsname, writeMode='a')
-t1 = time.clock()
-fps = img_to_take/ (t1 - t0)
-hz = (1./exptime)*1e6
-Mbps = (pxlx*pxly*hz*2.*8.)/1e6
-print ""
-print "Theory:  %dx%d = %.3f Hz.  %d num of img. Exptime: %d [us]. %.2f Mbps aprox" % (pxlx,pxly,hz, img_to_take,exptime, Mbps)
-print "Empirical: %dx%d = %.3f fps. %d num of img. Exptime: %d [us]" % (pxlx,pxly,fps, img_to_take, exptime)
+    print "New Exposure Time %d" % exptime
+    average = 0
+    t0 = time.clock()
+    for i in range(0, img_to_take+1):
+        #Preparing name for fits:
+        fitsname = "img_exp%s.fits" % str(exptime).zfill(2)
+#        print "Taking %d ...." % i
+        #Getting image from buffer
+        stream=d.GetStream('%srtcPxlBuf'%options.prefix)#a single frame
+        #change the default shape of image (default shape is usually: (x*y,1))
+        data = stream[0].reshape(2*pxlx,pxly)
+        average += data.mean()
+    #    data = stream[0].reshape(pxlx,pxly)
+        #writes a fits
+        FITS.Write(data, fitsname, writeMode='a')
+    average = average /float(img_to_take)
+    t1 = time.clock()
+    fps = img_to_take/ (t1 - t0)
+    fps_e.append(average)
+    hz = (1./exptime)*1e6
+    Mbps = (pxlx*pxly*hz*2.*8.)/1e6
+    print ""
+    print "Theory:  %dx%d = %.3f Hz.  %d num of img. Exptime: %d [us]. %.2f Mbps aprox" % (pxlx,pxly,hz, img_to_take,exptime, Mbps)
+    print "Empirical: %dx%d = %.3f fps. %d num of img. Exptime: %d [us]" % (pxlx,pxly,fps, img_to_take, exptime)
+    print "########"
+    print fps_e
 
--- src/plot_fps.py
+++ src/plot_fps.py
@@ -9,6 +9,13 @@ time_x = [3000,5000,10000,12000,15000]
 fps_t = []
 fps_e = []
 
+
+#Theory:  120x120 = 66.667 Hz.  3000 num of img. Exptime: 15000 [us]. 15.36 Mbps aprox
+#Empirical: 120x120 = 355.450 fps. 3000 num of img. Exptime: 15000 [us]
+#########
+m_120 = [1.3113589930555563, 1.7046389236111146, 2.834339270833337, 3.5200261458333322, 5.1562564467592731]
+
+
 #120x120
 #fps_t.append(333.333)
 #fps_e.append(487.805)
@@ -33,17 +40,29 @@ fps_e = []
 #fps_t.append(66.667)
 #fps_e.append(613.497)
 
+#Theory:  100x100 = 66.667 Hz.  3000 num of img. Exptime: 15000 [us]. 10.67 Mbps aprox
+#Empirical: 100x100 = 489.396 fps. 3000 num of img. Exptime: 15000 [us]
+########
+m_100 = [1.5993922499999951, 2.191089316666663, 3.7160825833333324, 4.4224262333333346, 5.6826870666666682]
+
+
 #50x50    
-fps_t.append(333.333)
-fps_e.append(990.099)
-fps_t.append(200.000)
-fps_e.append(970.874)
-fps_t.append(100.000)
-fps_e.append(917.431)
-fps_t.append(83.333)
-fps_e.append(909.091)
-fps_t.append(66.667)
-fps_e.append(917.431)
+#fps_t.append(333.333)
+#fps_e.append(990.099)
+#fps_t.append(200.000)
+#fps_e.append(970.874)
+#fps_t.append(100.000)
+#fps_e.append(917.431)
+#fps_t.append(83.333)
+#fps_e.append(909.091)
+#fps_t.append(66.667)
+#fps_e.append(917.431)
+
+#Theory:  50x50 = 66.667 Hz.  3000 num of img. Exptime: 15000 [us]. 2.67 Mbps aprox
+#Empirical: 50x50 = 779.221 fps. 3000 num of img. Exptime: 15000 [us]
+########
+m_50 = [1.6931037333333354, 2.4284506000000028, 4.0837678000000119, 4.8331458666666585, 5.8004594000000056]
+
 
 
 #hfmt = dates.DateFormatter(fmt)
@@ -53,12 +72,13 @@ fps_e.append(917.431)
 #ax.xaxis.set_major_formatter(hfmt)
 #plt.subplots_adjust(bottom=.3)
 #plt.xticks(rotation='vertical')
-p1 = plt.plot(time_x, fps_t,'gx',label='theoric')
-p2 = plt.plot(time_x, fps_e,'ro', label='empiric')
+p1 = plt.plot(time_x, m_50,'go-',label='50')
+p2 = plt.plot(time_x, m_100,'ro-', label='100')
+p3 = plt.plot(time_x, m_120,'yo-', label='120')
 #plt.title('120x120')
 #plt.title('100x100')
-plt.title('50x50')
-plt.ylabel('FPS')
+plt.title('Mean v/s T')
+plt.ylabel('Mean')
 plt.xlabel('Exp Time [us]')
 legend()
 plt.show()             
