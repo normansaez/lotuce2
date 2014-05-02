6fbf9fb25de073af7c31d03751d19781883c8086
Norman Saez <norman.saez@gmail.com>
2014-05-02 12:32:35 +0100

    ploting image in runtime

---
+++ src/img_plot.py
@@ -0,0 +1,16 @@
+from matplotlib.pylab import *
+import time                   
+import darc
+
+ion()                        
+prefix = "both"
+d = darc.Control(prefix)
+ncam = d.Get("ncam")
+px   = d.Get("npxlx")[0]
+py   = d.Get("npxly")[0]
+
+while(True):
+    stream=d.GetStream('%srtcPxlBuf'%prefix)
+    data = stream[0].reshape(ncam*px,py)
+    imgplot = plt.imshow(data)
+    draw()
