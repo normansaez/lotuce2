85be30c43a2e2505edb3c4d4692a4a7b3db60025
Norman Saez <nfsaez@uc.cl>
2014-05-21 18:29:14 -0400

    Adding scatter plot

--- src/gen_syn_spot.py
+++ src/gen_syn_spot.py
@@ -5,32 +5,28 @@ import FITS
 from scipy import signal, misc
 import matplotlib.pyplot as plt
 
-rad = 15
-subap = 50
+# syntetic img to be convolved
+rad = 10
+subap = 20
 y,x = np.ogrid[-subap: subap+1, -subap: subap+1]
 mask = x**2+y**2 <= pi*rad**2
 mask = mask*1
-#plt.figure(1)
-#imshow(mask)
-#show()
-f = FITS.Read('bob2000407.fits')[1]
-print f.shape
-manta76 = f[0:492,]
-print manta76.shape
-manta77 = f[492:984,]
-print manta77.shape
-#plt.figure(2)
-#imshow(manta76)
-#show()
-#plt.figure(3)
-#imshow(manta77)
-#show()
 
-correlacion = signal.fftconvolve(manta76,mask,mode='same')
-#plt.figure(4)
-#imshow(correlacion)
-#show()
-cy,cx = np.unravel_index(correlacion.argmax(),correlacion.shape)
-print cy,cx
+#getting img to be convolved
+f = FITS.Read('bob2000407.fits')[1]
+#split img per camera
+manta76 = f[0:492,]
+manta77 = f[492:984,]
+
+correlation = signal.fftconvolve(manta76,mask,mode='same')
+#Getting max 
+cy76,cx76 = np.unravel_index(correlation.argmax(),correlation.shape)
+print cy76,cx76
+# put a blue dot at (10, 20)
+plt.scatter([cx76], [cy76],c='r',s=40)
+
+# put a red dot, size 40, at 2 locations:
+#plt.scatter(x=[30, 40], y=[50, 60], c='r', s=40)
+
 imshow(manta76)
 plt.show()
