c61315155cc8ecbe1f457b7e45a281240ce0191b
Norman Saez <nfsaez@uc.cl>
2014-05-21 18:50:23 -0400

    Adding SubAp square

--- src/gen_syn_spot.py
+++ src/gen_syn_spot.py
@@ -4,11 +4,13 @@ from math import pi
 import FITS
 from scipy import signal, misc
 import matplotlib.pyplot as plt
+from matplotlib.path import Path
+import matplotlib.patches as patches
 
 # syntetic img to be convolved
-rad = 10
-subap = 20
-y,x = np.ogrid[-subap: subap+1, -subap: subap+1]
+rad = 5
+kernel = 20
+y,x = np.ogrid[-kernel: kernel+1, -kernel: kernel+1]
 mask = x**2+y**2 <= pi*rad**2
 mask = mask*1
 
@@ -18,15 +20,31 @@ f = FITS.Read('bob2000407.fits')[1]
 manta76 = f[0:492,]
 manta77 = f[492:984,]
 
-correlation = signal.fftconvolve(manta76,mask,mode='same')
+correlation = signal.fftconvolve(manta77,mask,mode='same')
 #Getting max 
-cy76,cx76 = np.unravel_index(correlation.argmax(),correlation.shape)
-print cy76,cx76
-# put a blue dot at (10, 20)
-plt.scatter([cx76], [cy76],c='r',s=40)
+cy77,cx77 = np.unravel_index(correlation.argmax(),correlation.shape)
+print cy77,cx77
+plt.scatter([cx77], [cy77],c='b',s=10)
+imshow(manta77)
+#Put subap
+subap = 60
+verts = [
+    (cx77-subap, cy77-subap), # left, bottom
+    (cx77-subap, cy77+subap), # left, top
+    (cx77+subap, cy77+subap), # right, top
+    (cx77+subap, cy77-subap), # right, bottom
+    (cx77, cy77), # ignored
+    ]
 
-# put a red dot, size 40, at 2 locations:
-#plt.scatter(x=[30, 40], y=[50, 60], c='r', s=40)
+codes = [Path.MOVETO,
+         Path.LINETO,
+         Path.LINETO,
+         Path.LINETO,
+         Path.CLOSEPOLY,
+         ]
 
-imshow(manta76)
+path = Path(verts, codes)
+
+patch = patches.PathPatch(path, facecolor='none', EdgeColor='red', lw=1)
+plt.gca().add_patch(patch)
 plt.show()
