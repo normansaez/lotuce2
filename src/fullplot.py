b5aeea6a3d2aa781d24ab4aa431671a0623435c1
Norman Saez <nfsaez@uc.cl>
2014-05-22 18:04:56 -0400

    rename var and methods in get_syn_spot get images from one image only

--- src/fullplot.py
+++ src/fullplot.py
@@ -7,8 +7,8 @@ import glob
 import time
 
 all_fits = glob.glob('*.fits')
-plt.ion()
-plt.show()
+#plt.ion()
+#plt.show()
 plt.gca().invert_yaxis()
 #plt.autoscale(False)
 for fits_name in all_fits:
@@ -33,7 +33,8 @@ for fits_name in all_fits:
     x = range(0,656)
     plt.subplot(221)
     p1 = plt.plot(x,ny,'-')
-    plt.draw()
-    time.sleep(0.5)
-
+#    plt.draw()
+#    time.sleep(0.5)
+    plt.show()
+#    time.sleep(10.5)
     
--- src/gen_syn_spot.py
+++ src/gen_syn_spot.py
@@ -8,12 +8,12 @@ from matplotlib.path import Path
 import matplotlib.patches as patches
 import time
 
-def get_subap_square(cx, cy, subap,color='red'):
+def get_square(cx, cy, side, color='red'):
     verts = [
-        (cx-subap, cy-subap), # left, bottom
-        (cx-subap, cy+subap), # left, top
-        (cx+subap, cy+subap), # right, top
-        (cx+subap, cy-subap), # right, bottom
+        (cx-side, cy-side), # left, bottom
+        (cx-side, cy+side), # left, top
+        (cx+side, cy+side), # right, top
+        (cx+side, cy-side), # right, bottom
         (cx, cy), # ignored
         ]
     
@@ -35,12 +35,10 @@ def get_centroid(img, mask):
     cy, cx = np.unravel_index(correlation.argmax(),correlation.shape)
     return cy,cx
 
-def get_mask(rad=5, kernel=20):
+def get_mask_spot(radio=5, kernel=20):
     # syntetic img to be convolved
-    rad = 5
-    kernel = 20
     y,x = np.ogrid[-kernel: kernel+1, -kernel: kernel+1]
-    mask = x**2+y**2 <= pi*rad**2
+    mask = x**2+y**2 <= pi*radio**2
     mask = mask*1
     return mask
 
@@ -51,26 +49,31 @@ if __name__ == '__main__':
     #split img per camera
     manta76 = f[0:492,]
     manta77 = f[492:984,]
-    #get mask
-    mask = get_mask(rad=5,kernel=20)
-    #img1
+    weight = 120
+    height = weight
     subap = 60
+    radio = 5
+    kernel = 20
+    #get mask
+    mask = get_mask_spot(radio,kernel)
+    #img1
     img = manta77
     cy, cx = get_centroid(img, mask)
-    plt.scatter([cx], [cy],c='b',s=10)
-    patch = get_subap_square(cx,cy,subap)
+    #plt.scatter([cx], [cy],c='b',s=10)
+    patch = get_square(cx,cy,subap)
     plt.gca().add_patch(patch)
-    patch = get_subap_square(cx,cy,subap+20,color='green')
+    patch = get_square(cx,cy,height,color='green')
     plt.gca().add_patch(patch)
     imshow(img)
+########################################
     plt.figure(2)
-    subap = 60
+    #img1
     img = manta76
     cy, cx = get_centroid(img, mask)
-    plt.scatter([cx], [cy],c='b',s=10)
-    patch = get_subap_square(cx,cy,subap)
+    #plt.scatter([cx], [cy],c='b',s=10)
+    patch = get_square(cx,cy,subap)
     plt.gca().add_patch(patch)
-    patch = get_subap_square(cx,cy,subap+20,color='green')
+    patch = get_square(cx,cy,height,color='green')
     plt.gca().add_patch(patch)
     imshow(img)
     print time.time() - start_time, "seconds"
