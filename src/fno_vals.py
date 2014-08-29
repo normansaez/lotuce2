1df5a2a6984c2b0932843ebf69fe84b6239b9844
Norman Saez <nfsaez@uc.cl>
2014-08-29 16:05:05 -0400

    The other thing to look is camera synchronisation. May not be synchronised properly, i.e. may be a frame out of sync with each other. See results on commented block.

--- src/fno_vals.py
+++ src/fno_vals.py
@@ -6,3 +6,13 @@ print fno[1:]-fno[:-1]
 timestamp=data["rtcPxlBuf"][1]
 print timestamp
 print timestamp[1:]-timestamp[:-1]
+
+#pxls=data["rtcPxlBuf"][0]
+#s1=pxls[:,:pxls.size/2].sum(1)
+#s2=pxls[:,pxls.size/2:].sum(1)
+#
+#Then have a look at s1 and s2:
+#pylab plot(s1)
+#pylab.plot(s2)
+#pylab.show()
+
