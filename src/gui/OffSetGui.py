281fe6766adbd61052b3b8cf0a27df9b2144117a
Norman Saez <norman.saez@gmail.com>
2014-10-24 15:24:23 -0300

    Fixing Offset GUI with GTK 3

--- src/gui/OffSetGui.py
+++ src/gui/OffSetGui.py
@@ -1,50 +1,43 @@
 #!/usr/bin/env python
 import os
 import sys
-import pygtk  
+#import pygtk  
 #pygtk.require("2.0")  
-import gtk  
-
+#import gtk  
+from gi.repository import Gtk
+#from gi.repository import GObject
 
 
 class BeagleDarcGui:
 
-    wTree = gtk.Builder()
 
     def __init__( self ):
         path, fil = os.path.split(os.path.abspath(__file__))
 
-        self.builder = gtk.Builder()
+        self.builder = Gtk.Builder()
         self.builder.add_from_file(path+"/offset.glade")
         self.window = self.builder.get_object ("window1")
-        self.window.set_events(self.window.get_events() | gtk.gdk.BUTTON_PRESS_MASK)
+        self.window.set_events(self.window.get_events())
 
-#        if self.window:
-#            self.window.connect("destroy", gtk.main_quit)
+        if self.window:
+            self.window.connect("destroy", Gtk.main_quit)
 
-#        #Toggle button to connect to beaglebone
-#        self.connect_togglebutton = self.builder.get_object ("connect_togglebutton")
+        #Toggle button to connect to beaglebone
+        self.connect_togglebutton = self.builder.get_object ("connect_togglebutton")
 #        self.connect_togglebutton.connect("toggled", self.callback, "Connection")
-#
-#        #default entries
-#        self.entry1 = self.builder.get_object("entry1")
-#        self.entry2 = self.builder.get_object("entry2")
-#        self.entry3 = self.builder.get_object("entry3")
-#        self.image4 = self.builder.get_object("image4")
-#
-#        self.entry1.set_text(self.bds.host ) 
-#        self.entry2.set_text(self.bds.user ) 
-#        self.entry3.set_text(self.bds.ior) 
-#
-#        dic = { 
-#            "on_buttonQuit_clicked" : self.quit,
-#            "on_window1_destroy" : self.quit,
-#            "gtk_widget_destroy" : self.quit,
-#            "on_phasescreen_menuitem_activate" : self.phasescreen,
-#            "on_stars_menuitem_activate" : self.stars
-#        }
-#        
-#        self.builder.connect_signals( dic )
+
+        #default entries
+        self.entry1 = self.builder.get_object("entry1")
+        self.entry2 = self.builder.get_object("entry2")
+        self.entry3 = self.builder.get_object("entry3")
+        self.image4 = self.builder.get_object("image4")
+
+
+        dic = { 
+            "on_buttonQuit_clicked" : self.quit,
+        }
+        
+        self.builder.connect_signals( dic )
 
     def callback(self, widget, data=None):
         '''
@@ -57,11 +50,6 @@ class BeagleDarcGui:
             widget.set_use_stock(True)
             self.image4.set_from_stock(gtk.STOCK_CONNECT, gtk.ICON_SIZE_MENU)
             self.bds.ior = self.entry3.get_text()
-            #Layers
-            #LayersGui.Layers()
-            #LayerData.LayerData()
-            #StarsGui.Main()
-            #StarData.StarData()
 
         if widget.get_active() is False:
             widget.set_label(gtk.STOCK_CONNECT)
@@ -72,14 +60,6 @@ class BeagleDarcGui:
         sys.exit(0)
 
 
-    def phasescreen(self, widget):
-        LayersGui.Layers()
-        LayerData.LayerData()
-
-    def stars(self, widget):
-        StarsGui.Main()
-        StarData.StarData()
-
 BeagleDarcGui = BeagleDarcGui()
 BeagleDarcGui.window.show()
-gtk.main()
+Gtk.main()
