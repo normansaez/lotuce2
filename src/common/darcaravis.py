dfcec8b31241739c0121c69e98867f01db4064d3
Norman Saez <norman.saez@gmail.com>
2014-10-26 11:08:48 -0300

    Adding timeout to get instance

--- src/common/darcaravis.py
+++ src/common/darcaravis.py
@@ -1,28 +1,33 @@
 import darc
 import sys
+import signal
+import time
 
 class DarcAravis:
     def __init__(self, prefix):
-#        try:
-#            darc_instance = darc.Control(prefix)
-#        except Exception, e:
-#            print e
         pass
+#        signal.signal(signal.SIGALRM, self.handler)
+#        signal.alarm(5)
+#        self.darc_instance = darc.Control(prefix)
+#        time.sleep(10)               
+
+#    def handler(self, signum, frame):
+#        raise Exception("timeout getting darc!")
 
     def set(self, camera, parameter, value):
         cam = "aravisCmd%d" % camera
         cmd = '%s=%s;' % (parameter, str(value))
-        darc_instance.Set(cam, cmd)
+        self.darc_instance.Set(cam, cmd)
         print sys._getframe().f_code.co_name,
         print "Cam%d => %s: %s" % (camera, parameter, str(value))
     
     def get(self, camera, parameter):
         cmd = "?%d:%s" % (camera, parameter)
-        darc_instance.Set('aravisGet',cmd)
+        self.darc_instance.Set('aravisGet',cmd)
         try:
-            result = int(darc_instance.Get("aravisGet"))
+            result = int(self.darc_instance.Get("aravisGet"))
         except ValueError, e:
-            result = darc_instance.Get("aravisGet")
+            result = self.darc_instance.Get("aravisGet")
             print sys._getframe().f_code.co_name,
             print "Cam%d => %s: %s" % (cam, parameter, result)
             return result
