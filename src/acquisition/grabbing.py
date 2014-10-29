563c5f740c22c25a7682445fb9f28339cdba4672
Norman Saez <nfsaez@uc.cl>
2014-10-29 14:21:50 -0300

    takes darc Hz and use them to take long term data acquisition

--- src/acquisition/grabbing.py
+++ src/acquisition/grabbing.py
@@ -4,12 +4,14 @@ import time
 from optparse import OptionParser
 from pprint import pprint
 import numpy as np
+import re
 
 parse = OptionParser()
 #parse.add_option('-c', '--camera', dest='camera', type='str', help='Camera num: 0,1,2,3 etc', default="0")
 parse.add_option('-p', '--prefix', dest='prefix', type='str', help='Camera prefix: default all', default="both")
 parse.add_option('-e', '--exptime', dest='exptime', type=int, help='Value for ExposureTimeAbs', default=1000)
-parse.add_option('-i', '--nimg', dest='nimg', type=int, help='Number of images', default=1)
+#parse.add_option('-i', '--nimg', dest='nimg', type=int, help='Number of images', default=1)
+parse.add_option('-t', '--time', dest='time', type=int, help='Image time in seconds', default=60)
 (options , argv) = parse.parse_args()
 #Takes camera instance
 d=darc.Control(options.prefix)
@@ -17,13 +19,23 @@ d=darc.Control(options.prefix)
 pxlx =d.Get("npxlx")[0]
 pxly =d.Get("npxly")[0]
 
-img_to_take =  options.nimg
+info,ftime,fno=d.GetStream(options.prefix+"rtcStatusBuf")
+line = info.tostring()
+sts = re.search(r'Frame time .*s (.*)', line, re.M|re.I)
+try:
+    HZ_str = sts.group(1) 
+    print "Hz from darc: %s"% HZ_str
+    hz = float(HZ_str.split('(')[1].split('Hz)')[0])
+except:
+    pass
+print "number of images for %f [Hz] and %d time: %d [seconds]" % (hz, options.time,int(hz*options.time))
+img_to_take =  int(hz*options.time)#options.nimg
 exptime = options.exptime
 d.Set("aravisCmdAll",'ExposureTimeAbs=%d;'% exptime)
 d.Set("aravisGet","?0:ExposureTimeAbs")
 exptime=int(d.Get("aravisGet"))
 t0 = time.clock()
-streamBlock = d.GetStreamBlock('%srtcPxlBuf'%options.prefix,img_to_take,block=1,flysave='something.fits')
+streamBlock = d.GetStreamBlock('%srtcPxlBuf'%options.prefix,img_to_take,block=1,flysave='img.fits')
 t1 = time.clock()
 print "%f" % (t1-t0)
 streams = streamBlock['%srtcPxlBuf'%options.prefix]
