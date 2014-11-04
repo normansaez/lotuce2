dfb2f092a92c4788b563dad03bda9211b3fab877
Norman Saez <nfsaez@uc.cl>
2014-11-04 18:00:51 -0300

    removing useles code

--- src/acquisition/profilesaver.c
+++ src/acquisition/profilesaver.c
@@ -33,43 +33,19 @@ saver (PyObject *dummy, PyObject *args)
     if (arr1 == NULL)
         return NULL;
 
-    //nd = PyArray_NDIM(arr1);   //number of dimensions
-    //double *da = (double *)PyArray_DATA(arr1);
     int nn = PyArray_SIZE(arr1);
-    //printf("elem: %d\n", nn);
-    //for (int i=0; i < nn; i++){
-    //    printf("%f\n",da[i]);
-    //}
-    //printf("%s\n",filename);
 
     f = fopen(filename, "wb"); // wb -write binary
     if (f != NULL) 
     {
-        //   printf("size: %lu\n",sizeof(arr1));
         fwrite(arr1, sizeof(arr1), nn, f);
         fclose(f);
     }
     else
-    {
-        //failed to create the file
-        printf("failed!\n");
-    }
-
-    //    f = fopen("profile.dat","rb");
-    //    while (!feof(f)) {
-    //        printf("1:ok here!");
-    //        fread(arr2, sizeof(arr2), 1, f);
-    //        printf("2:ok here!");
-    //        double *da = (double *)PyArray_DATA(arr2);
-    //        int nn = PyArray_SIZE(arr2);
-    //        for (int i=0; i < nn; i++){
-    //            printf("%f\n",da[i]);
-    //        }
-    //    }
-
+        printf("failed to create the file!\n");
 
     Py_DECREF(arr1);
-    return Py_None;//PyInt_FromLong(nd);
+    return Py_None;
 }
 
 static struct PyMethodDef methods[] = {
