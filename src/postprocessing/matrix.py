05fd345bb19f49c441b3e115738afb028c8c3c83
Norman Saez <nfsaez@uc.cl>
2015-01-23 14:43:13 -0300

    mean

--- src/postprocessing/matrix.py
+++ src/postprocessing/matrix.py
@@ -16,6 +16,22 @@ m = [[0,0,0,0],
 [0,0,0,0]]
 matrix = numpy.matrix(m)
 c = 0
+c01 = [] 
+c02 = [] 
+c03 = [] 
+
+c10 = [] 
+c12 = [] 
+c13 = [] 
+
+c20 = [] 
+c21 = [] 
+c23 = [] 
+
+c30 = [] 
+c31 = [] 
+c32 = []
+
 for line in filehandler:
     line = line.rstrip('\n').split(' ')
     ts = float(line[0])
@@ -27,20 +43,50 @@ for line in filehandler:
     if counter == 0:
         break
     counter -= 1
-    m = [[0,cam0-cam1,cam0-cam2,cam0-cam3],
-    [cam1-cam0,0,cam1-cam2,cam1-cam3],
-    [cam2-cam0,cam2-cam1,0,cam2-cam3],
-    [cam3-cam0,cam3-cam1,cam3-cam2,0]]
-    mtrx = numpy.matrix(m)
-    matrix = matrix + mtrx
+    c01.append(cam0-cam1) 
+    c02.append(cam0-cam2)
+    c03.append(cam0-cam3)
+
+    c10.append(cam1-cam0)
+    c12.append(cam1-cam2)
+    c13.append(cam1-cam3)
+
+    c20.append(cam2-cam0)
+    c21.append(cam2-cam1)
+    c23.append(cam2-cam3)
+
+    c30.append(cam3-cam0)
+    c31.append(cam3-cam1)
+    c32.append(cam3-cam2)
+
     c += 1
-print c
+
+c01 = numpy.array(c01)  
+c02 = numpy.array(c02)
+c03 = numpy.array(c03)
+
+c10 = numpy.array(c10)
+c12 = numpy.array(c12)
+c13 = numpy.array(c13)
+
+c20 = numpy.array(c20)
+c21 = numpy.array(c21)
+c23 = numpy.array(c23)
+
+c30 = numpy.array(c30)
+c31 = numpy.array(c31)
+c32 = numpy.array(c32)
+
+m = [[0,c01.mean(),c02.mean(),c03.mean()],
+[c10.mean(),0,c12.mean(),c13.mean()],
+[c20.mean(),c21.mean(),0,c23.mean()],
+[c30.mean(),c31.mean(),c32.mean(),0]]
+matrix = numpy.matrix(m)
 print matrix
-print matrix/c
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.set_aspect('equal')
-plt.imshow(matrix/c, interpolation='nearest', cmap=plt.cm.hot_r)
+plt.imshow(matrix, interpolation='nearest', cmap=plt.cm.hot_r)
 plt.colorbar()
 labels = range(0, len(m[0]))
 plt.xticks(labels)
