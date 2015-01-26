e721dad71f788d38000d0a4f4e4902d54c497809
Norman Saez <nfsaez@uc.cl>
2015-01-26 16:40:35 -0300

    Adding time at the end of the execution

--- src/acquisition/grabbing.py
+++ src/acquisition/grabbing.py
@@ -58,6 +58,7 @@ t0 = time.clock()
 streamBlock = d.GetStreamBlock('%srtcPxlBuf'%options.prefix,img_to_take,block=1,flysave=options.directory+'/img.fits')
 t1 = time.clock()
 print "%f time taken, time commanded %d" % ((t1-t0), options.time)
+print "number of images for %f [Hz] and %d time[seconds]: %d images to take" % (hz, options.time,int(hz*options.time))
 print "Images stored in : %s" % options.directory
 
 #streams = streamBlock['%srtcPxlBuf'%options.prefix]
