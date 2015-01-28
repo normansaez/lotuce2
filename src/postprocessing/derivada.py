cfeb5bbf8e1fc9dd3e33d2b95c5acd371a3bcade
Norman Saez <nfsaez@uc.cl>
2015-01-28 15:04:20 -0300

    adding limit to be plot

--- src/postprocessing/derivada.py
+++ src/postprocessing/derivada.py
@@ -1,24 +1,17 @@
-import FITS
-import glob
 import sys
 import os
 from pylab import grid#imshow,show
-from pylab import xticks
+#from pylab import xticks
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import argparse 
 import numpy as np
 from math import floor
 from matplotlib import ticker
-#COLOR CONST
-BLUE     = '\033[34m'
-RED      = '\033[31m'
-GREEN    = '\033[32m'
-YELLOW   = '\033[33m'
-BLACK    = '\033[30m'
-CRIM     = '\033[36m'
-NO_COLOR = '\033[0m'
+#from matplotlib import rcParams
 
+#rcParams.update({'font.size': 18, 'text.usetex': True})
+#rcParams.update({'font.size': 18, 'font.family': 'STIXGeneral', 'mathtext.fontset': 'stix'})
 
 if __name__=="__main__":
     print "Calibrating centroid pattern ..."
@@ -26,6 +19,7 @@ if __name__=="__main__":
     '''
     parser = argparse.ArgumentParser(usage=usage)
     parser.add_argument('-d', '--filename', dest='filename', type=str, help='Path to get images', default=None)
+    parser.add_argument('-l', '--limit', dest='limit', type=int, help='Default limit to plot', default=393300)#786600)#None)
 
     (options, unknown) = parser.parse_known_args()
 
@@ -55,17 +49,19 @@ for line in filehandler:
     fnos.append(fno)
 #    axis_x.append(i)
     i += 1
+if options.limit is not None:
+    i = options.limit
 axis_x = [j for j in range(0,i-1)]
 np_fnos = np.array(fnos)
 print (np_fnos[1:]-np_fnos[:-1]).max()
 print (np_fnos[1:]-np_fnos[:-1]).min()
 
-for i in range(0,len(np_fnos)-1):
-    fno_id = np_fnos[i+1] - np_fnos[i]
+for j in range(0,i-1):
+    fno_id = np_fnos[j+1] - np_fnos[j]
     fns.append(fno_id)
 fig = plt.figure()
 ax = plt.subplot(111)
-ax.plot(axis_x, fns, 'r.',label=r'$\Delta id(n)$')
+ax.plot(axis_x, fns,'r.', label=r'$\Delta id(n)$')
 box = ax.get_position()
 ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
 plt.title(r'img v.s $\Delta id(n)$ %s' % (basename))
