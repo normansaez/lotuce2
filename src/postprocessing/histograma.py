65ad7676fa17682aa769b359c36aa62472c4c272
Norman Saez <nfsaez@uc.cl>
2015-02-05 18:00:31 -0300

    histogram in log

--- src/postprocessing/histograma.py
+++ src/postprocessing/histograma.py
@@ -55,10 +55,29 @@ del axis_x[-1]
 np_fnos = np.array(fnos)
 print (np_fnos[1:]-np_fnos[:-1]).max()
 print (np_fnos[1:]-np_fnos[:-1]).min()
+c_01 = 0
+c_2_100 = 0
+c_100_200 = 0
+c_200_500 = 0
+c_500_1000= 0
+c_1000_3000 = 0
 
 for j in range(0,i-1):
     fno_id = np_fnos[j+1] - np_fnos[j]
     fns.append(fno_id)
+    if fno_id <= 1:
+        c_01 += 1
+    if fno_id > 1 and fno_id <= 100:
+        c_2_100 += 1
+    if fno_id > 100 and fno_id <= 200:
+        c_100_200 += 1
+    if fno_id > 200 and fno_id <= 500:
+        c_200_500 += 1
+    if fno_id > 500 and fno_id <= 1000:
+        c_500_1000 += 1
+    if fno_id > 1000 and fno_id <= 3000:
+        c_1000_3000 += 1
+
 print len(axis_x)
 print len(fns)
 #ploy
@@ -68,8 +87,25 @@ print npfns.min()
 print npfns.max()
 print npfns.mean()
 print npfns.std()
+print "ranges"
+print c_01
+print c_2_100
+print c_100_200
+print c_200_500
+print c_500_1000
+print c_1000_3000
+print "ranges"
+t = len(fns)*1.
+print (c_01/t)*100.
+print (c_2_100/t)*100.
+print (c_100_200/t)*100.
+print (c_200_500/t)*100.
+print (c_500_1000/t)*100.
+print (c_1000_3000/t)*100.
+
+
 from math import ceil
-bins = range(0,150,5)
+bins = 20#range(0,3000,10)#[0,10,100,200,500,100,3000]#range(0,200,10)
 print bins
-hist(fns,bins,normed=True)
+hist(fns,bins,normed=True,log=True)
 show()
