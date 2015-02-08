fe259233a83baa8014c03d25791def6c95fec0c9
Norman Saez <nfsaez@uc.cl>
2015-02-08 02:08:23 -0300

    bash to get all

---
+++ src/postprocessing/data-lost/dt_l.bash
@@ -0,0 +1,8 @@
+python data-loss.py  -f /Users/nsaez/datasets/A/lotuce2-run-results-2015_01_23.11.txt &
+python data-loss.py  -f /Users/nsaez/datasets/B/lotuce2-run-results-2015_01_24.12.txt &
+python data-loss.py  -f /Users/nsaez/datasets/C/lotuce2-run-results-2015_02_05.1.txt &
+python data-loss.py  -f /Users/nsaez/datasets/D/lotuce2-run-results-2015_02_05.0.txt &
+python data-loss.py  -f /Users/nsaez/datasets/E/lotuce2-run-results-2015_02_06.2.txt &
+python data-loss.py  -f /Users/nsaez/datasets/F/lotuce2-run-results-2015_02_04.0.txt &
+python data-loss.py  -f /Users/nsaez/datasets/G/lotuce2-run-results-2015_02_04.1.txt &
+python data-loss.py  -f /Users/nsaez/datasets/H/lotuce2-run-results-2015_02_05.2.txt &
