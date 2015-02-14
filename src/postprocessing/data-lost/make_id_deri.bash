957b54eed4987584057b5b2d313fc66ca4f6636b
Norman Saez <nfsaez@uc.cl>
2015-02-13 22:50:04 -0300

    moving bash to makefile

--- src/postprocessing/data-lost/make_id_deri.bash
+++
@@ -1,5 +0,0 @@
-#python id_deri_vs_time.py -f /Users/nsaez/datasets/A/lotuce2-run-results-2015_01_23.11.txt &
-#python id_deri_vs_time.py -f /Users/nsaez/datasets/B/lotuce2-run-results-2015_01_24.12.txt &
-python pd_id_deri_vs_time.py -f /Users/nsaez/datasets/C/lotuce2-run-results-2015_02_05.1.txt -s /Users/nsaez/datasets/F/lotuce2-run-results-2015_02_04.0.txt &
-python pd_id_deri_vs_time.py -f /Users/nsaez/datasets/D/lotuce2-run-results-2015_02_05.0.txt -s /Users/nsaez/datasets/G/lotuce2-run-results-2015_02_04.1.txt &
-python pd_id_deri_vs_time.py -f /Users/nsaez/datasets/E/lotuce2-run-results-2015_02_06.2.txt -s /Users/nsaez/datasets/H/lotuce2-run-results-2015_02_05.2.txt &
