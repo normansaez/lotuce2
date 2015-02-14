8c63322c08c95b6316083e21e0a289e81d3100b8
Norman Saez <nfsaez@uc.cl>
2015-02-13 22:51:52 -0300

    moving bash to makefile

--- src/postprocessing/data-lost/make_hist.bash
+++
@@ -1,4 +0,0 @@
- python histograma.py -e C-F -f ~/datasets/C/lotuce2-run-results-2015_02_05.1.txt -s ~/datasets/F/lotuce2-run-results-2015_02_04.0.txt &
- python histograma.py -e D-G -f ~/datasets/D/lotuce2-run-results-2015_02_05.0.txt -s ~/datasets/G/lotuce2-run-results-2015_02_04.1.txt &
- python histograma.py -e E-H -f ~/datasets/E/lotuce2-run-results-2015_02_06.2.txt -s ~/datasets/H/lotuce2-run-results-2015_02_05.2.txt &
-
