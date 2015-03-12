0e77914371f9d92cd89b3dc902edc4269113b11a
Norman Saez <nfsaez@uc.cl>
2015-03-12 01:28:58 -0300

    Adding test for full profile. 4 pixels are darc just before start next camera image

---
+++ src/tests/full_profile.py
@@ -0,0 +1,32 @@
+#import gtk
+import os
+#from matplotlib.backends.backend_gtkagg import FigureCanvasGTKAgg as FigureCanvas
+import numpy as np
+#from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+import FITS
+cam0_nx = np.array([])
+cam0_ny = np.array([])
+#f = FITS.Read('full.fits')#'800x200.fits')
+f = FITS.Read('800x200.fits')
+data = f[1]
+print data.shape
+x = len(data)
+axis = range(0,x)
+for i in range(0,x):
+    cam0_nx = np.append(cam0_nx, data[i,].sum())
+cam0_fx = plt.figure()#Figure(figsize=(5,4), dpi=30)
+ax = cam0_fx.add_subplot(111)
+ax.set_ylim(0,x)
+ax.invert_xaxis()
+ax.plot(cam0_nx,axis,'-')
+plt.show()
+count = 0
+for i in axis:
+
+    if cam0_nx[i] == 0:
+        print "%d" % i
+        count +=1
+        if count == 4:
+            count = 0
+            print
