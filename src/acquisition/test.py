e92c45f40a0a6aff7e551c028bd8da454de37afe
Norman Saez <nfsaez@uc.cl>
2015-03-12 01:27:48 -0300

    stuff to get fit quickly

--- src/acquisition/test.py
+++ src/acquisition/test.py
@@ -1,10 +1,23 @@
-import numpy
-from profilesaver import saver
-a = numpy.array([11,22,9,875,26,22,88900,1,2,3,4,56,7,8,00])
-print "%d => file should be %d" %(len(a), (len(a))*8)
-print a
-saver(a,'test.bin')
-print "######################"
-from profilesaver import hydrate
-b = hydrate('test.bin')
-print b
+import FITS
+import darc
+import time
+import argparse 
+from pprint import pprint
+import numpy as np
+import re
+import os
+import glob
+
+d=darc.Control('all')
+#takes camera pixels (x,y)
+pxlx =d.Get("npxlx")[0]
+pxly =d.Get("npxly")[0]
+
+streamBlock = d.GetStreamBlock('%srtcPxlBuf'%'all',1)#,block=1,flysave=options.directory+'/img.fits')
+streams = streamBlock['%srtcPxlBuf'%'all']
+count = 0
+for stream in streams:
+    data = stream[0].reshape((4*pxly,pxlx))
+    fitsname = 'full.fits'#options.directory+"/img_%s.fits" % (str(count).zfill(3))
+    FITS.Write(data, fitsname, writeMode='w')
+    count += 1
