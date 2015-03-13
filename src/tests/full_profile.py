a4433233deb6285789faa11e7b020adc886fe33c
Norman Saez <nfsaez@uc.cl>
2015-03-13 18:58:17 -0300

    fix

--- src/tests/full_profile.py
+++ src/tests/full_profile.py
@@ -7,14 +7,24 @@ import matplotlib.pyplot as plt
 import FITS
 cam0_nx = np.array([])
 cam0_ny = np.array([])
-#f = FITS.Read('full.fits')#'800x200.fits')
-f = FITS.Read('800x200.fits')
+f = FITS.Read('~/full.fits')#'800x200.fits')
+#f = FITS.Read('~/800x200.fits')
 data = f[1]
 print data.shape
 x = len(data)
 axis = range(0,x)
 for i in range(0,x):
     cam0_nx = np.append(cam0_nx, data[i,].sum())
+cam0_nx = cam0_nx/4095.
+count = 0
+for i in axis:
+    if cam0_nx[i] == 0:
+        print "%d" % i
+        count +=1
+        cam0_nx[i] = 0.07
+        if count == 4:
+            count = 0
+            print
 cam0_fx = plt.figure()#Figure(figsize=(5,4), dpi=30)
 ax = cam0_fx.add_subplot(111)
 ax.set_ylim(0,x)
@@ -22,11 +32,12 @@ ax.invert_xaxis()
 ax.plot(cam0_nx,axis,'-')
 plt.show()
 count = 0
-for i in axis:
+delta = np.diff(cam0_nx)
+axis = range(0, len(delta))
+cam0_fx = plt.figure()#Figure(figsize=(5,4), dpi=30)
+ax = cam0_fx.add_subplot(111)
+ax.set_ylim(0,x)
+ax.invert_xaxis()
+ax.plot(delta,axis,'-')
+plt.show()
 
-    if cam0_nx[i] == 0:
-        print "%d" % i
-        count +=1
-        if count == 4:
-            count = 0
-            print
