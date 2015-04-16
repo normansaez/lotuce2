97947680d63765541b77a440259de8dca0cd2bc2
Norman Saez <nfsaez@uc.cl>
2015-04-16 12:21:24 -0300

    updating time in beaglebone in order to compile it correctly

--- src/setup/set_frequency.py
+++ src/setup/set_frequency.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import sys
 import os
-
+import datetime
 if __name__ == '__main__':
+    a = datetime.datetime
+    now = a.now()
     hz = sys.argv[1]
     if hz.isdigit():
+        cmd = """ssh ubuntu@192.168.0.203 'sudo date -s "%s"'""" % now.strftime("%d %b %Y %H:%M:%S")
+        os.system(cmd)
         cmd =   'ssh ubuntu@192.168.0.203 "sudo python /home/ubuntu/bbb-pru-clock/blinkled.py %s"' % hz
         os.system(cmd)
