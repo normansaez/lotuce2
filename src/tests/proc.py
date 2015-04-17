8ad22a7c098e24d48f074838b21d326958748a0a
Norman Saez <nfsaez@uc.cl>
2015-04-17 00:29:21 -0300

    Adding process handling. between daemon and grab

---
+++ src/tests/proc.py
@@ -0,0 +1,38 @@
+from multiprocessing import Process
+import time
+import signal
+import os
+import time
+import sys
+
+def receive_signal(signum, stack):
+    print 'Received:', signum
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, receive_signal)
+print 'My PID is:', os.getpid()
+
+def grabb(name):
+    for i in range(5):
+        print "grabb %d" % i 
+        time.sleep(0.5)
+
+def daemon(name):
+    i = 0
+    while True:
+        print "daemon %d" % i 
+        i += 1
+        time.sleep(1)
+
+if __name__ == '__main__':
+    p = Process(target=grabb, args=('',))
+    p.start()
+    print "l1"
+
+    p1 = Process(target=daemon, args=('',))
+    p1.start()
+    print "l2"
+    p.join()
+    if p.is_alive() is False:
+        p1.terminate()
