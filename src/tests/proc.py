be40602f0ee275a6cf4acd5ca5d230cac69fa694
Norman Saez <nfsaez@uc.cl>
2015-04-17 11:15:51 -0300

    more tests

--- src/tests/proc.py
+++ src/tests/proc.py
@@ -17,37 +17,28 @@ signal.signal(signal.SIGINT, receive_signal)
 print 'My PID is:', os.getpid()
 
 def grabb(name):
-##    for i in range(5):
-#    i = 0
-#    while True:
-#        print "grabb %d" % i 
-#        time.sleep(0.5)
-#        i += 1
     cmd = 'while true; do echo "grabbing"; sleep 1; done'
-#    cmd ='for i in $(ls);do echo $i;sleep(10);done'
     process = Popen(cmd , stdout=PIPE , stderr=PIPE , shell=True)
+    print process.pid
     process.wait()
+
 def daemon(name):
-#    i = 0
-#    while True:
-#        print "daemon %d" % i 
-#        i += 1
-#        time.sleep(1)
     cmd = 'while true; do echo "daemon"; sleep 2; done'
     process = Popen(cmd , stdout=PIPE , stderr=PIPE , shell=True)
+    print process.pid
+    process.terminate()
     process.wait()
 
 if __name__ == '__main__':
     p = Process(target=grabb, args=('',))
     p.start()
     pid = p
-    print "l1"
 
     p1 = Process(target=daemon, args=('',))
     p1.start()
     print pid
-    print "l2"
     p.join()
     if p.is_alive() is False:
         p1.terminate()
         print "finish because finish"
+
