f1a7f57c149d3c453e48b6919f95a0a1792b70ac
Norman Saez <nfsaez@uc.cl>
2015-04-19 19:12:21 -0300

    Adding cov

--- src/acquisition/centroid.py
+++ src/acquisition/centroid.py
@@ -17,10 +17,18 @@ streamBlock = d.GetStreamBlock('%srtcCentBuf'%prefix,5)#,block=1,flysave=options
 streams = streamBlock['%srtcCentBuf'%prefix]
 print len(streams)
 print "###"
+x0 = np.array([])
+x1 = np.array([])
 for stream in streams:
     print stream[0]
     print type(stream[0])
     print stream[0].shape
     data = stream[0]
-    fitsname = 'centroid.fits'#options.directory+"/img_%s.fits" % (str(count).zfill(3))
-    FITS.Write(data, fitsname, writeMode='w')
+#    fitsname = 'centroid.fits'#options.directory+"/img_%s.fits" % (str(count).zfill(3))
+#    FITS.Write(data, fitsname, writeMode='w')
+    x0 = np.append(x0,data[0])
+    x1 = np.append(x1,data[2])
+print x0
+print x1
+print "-----"
+print np.cov(x0,x1)[0][1]
