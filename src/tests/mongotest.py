f01a8518e7a7b6f6c48ca20db1e93f8f456bfb09
Norman Saez <nfsaez@uc.cl>
2015-04-28 13:27:27 -0300

    First query

--- src/tests/mongotest.py
+++ src/tests/mongotest.py
@@ -3,6 +3,8 @@
 import cPickle as pickle
 import pymongo
 import datetime
+from bson.objectid import ObjectId
+import time
 
 if __name__ == '__main__':
     client = pymongo.MongoClient()
@@ -22,4 +24,11 @@ if __name__ == '__main__':
     print ts0
     print ts1
 
-    
+    sec = 15
+#    gen_time = datetime.datetime.today() - datetime.timedelta(sec=sec) 
+    gen_time = ts1 - datetime.timedelta(seconds=sec) 
+#    dummy_id = ObjectId.from_datetime(gen_time)
+    print gen_time
+    ttt = time.mktime(gen_time.timetuple())
+    result = list(db.profiles.find({"timestamp": {"$gte": ttt}}))
+    print len(result)
