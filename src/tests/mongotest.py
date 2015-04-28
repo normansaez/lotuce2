bc0e51a979e7e5b1ef3ec4e4e243f84cf08e47ad
Norman Saez <nfsaez@uc.cl>
2015-04-28 12:47:44 -0300

    Get from timestamp

--- src/tests/mongotest.py
+++ src/tests/mongotest.py
@@ -2,14 +2,24 @@
 
 import cPickle as pickle
 import pymongo
-client = pymongo.MongoClient()
-db = client.lotuce2
-#db.profiles.count()
-one = db.profiles.find_one(sort=[("timestamp", 1 )])
-#mongo = pymongo.MongoClient()
-#collection = mongo['lotuce2']['profiles']
-#collection.ensure_index('timestamp')
-#last = db.profiles.find_one(sort=[("timestamp", -1 )])
-y = pickle.loads( one['profile'] )
-print y.max()
-print y.min()
+import datetime
+
+if __name__ == '__main__':
+    client = pymongo.MongoClient()
+    db = client.lotuce2
+    #db.profiles.count()
+    one = db.profiles.find_one(sort=[("timestamp", 1 )])
+    #mongo = pymongo.MongoClient()
+    #collection = mongo['lotuce2']['profiles']
+    #collection.ensure_index('timestamp')
+    last = db.profiles.find_one(sort=[("timestamp", -1 )])
+    #y = pickle.loads( one['profile'] )
+    #print y.max()
+    #print y.min()
+    
+    ts0 = datetime.datetime.fromtimestamp(one['timestamp'])
+    ts1 = datetime.datetime.fromtimestamp(last['timestamp'])
+    print ts0
+    print ts1
+
+    
