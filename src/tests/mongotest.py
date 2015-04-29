6b5c0e438c2e0c3e5a67926a0d41adb43995557e
Norman Saez <nfsaez@uc.cl>
2015-04-28 21:25:58 -0300

    tests

--- src/tests/mongotest.py
+++ src/tests/mongotest.py
@@ -32,3 +32,25 @@ if __name__ == '__main__':
     ttt = time.mktime(gen_time.timetuple())
     result = list(db.profiles.find({"timestamp": {"$gte": ttt}}))
     print result[0]['timestamp']
+    centro = db.centroid.find_one(sort=[('timestamp',1)])
+    print centro
+    ftime = time.mktime(datetime.datetime.today().timetuple())
+    data = []
+    data.append(pickle.loads(centro["x0"])) 
+    data.append(pickle.loads(centro["y0"]))
+    data.append(pickle.loads(centro["x1"]))
+    data.append(pickle.loads(centro["y1"]))
+    data.append(pickle.loads(centro["x2"]))
+    data.append(pickle.loads(centro["y2"]))
+    data.append(pickle.loads(centro["x3"]))
+    data.append(pickle.loads(centro["y3"]))
+    db.centroid.insert({"timestamp":ftime,"x0":float(data[0]) 
+                                         ,"y0":float(data[1])  
+                                         ,"x1":float(data[2])
+                                         ,"y1":float(data[3])
+                                         ,"x2":float(data[4])
+                                         ,"y2":float(data[5])
+                                         ,"x3":float(data[6])
+                                         ,"y3":float(data[7])})
+    print db.profiles.find_one(sort=[("timestamp", -1 )])
+     
