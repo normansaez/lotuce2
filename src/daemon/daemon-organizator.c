8155b630d15513e1a0019c3ee7c5f7dea9dfc618
Norman Saez <nfsaez@uc.cl>
2015-05-01 16:57:57 -0300

    Adding C stuff

--- src/daemon/daemon-organizator.c
+++ src/daemon/daemon-organizator.c
@@ -1,15 +1,15 @@
-/* localtime example */
 #include <stdio.h>
 #include <time.h>
 
 int main ()
 {
-  time_t rawtime;
-  struct tm * timeinfo;
+    time_t rawtime;
+    struct tm * timeinfo;
+    char buffer[26];
 
-  time ( &rawtime );
-  timeinfo = localtime ( &rawtime );
-  printf ( "Current local time and date: %s", asctime (timeinfo) );
-
-  return 0;
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(buffer, 26, "%Y_%m_%dT%H_%M", timeinfo);/*:%S", timeinfo);*/
+    printf("%s\n", buffer);
+    return 0;
 }
