d018b0fb28e0b60ec1b708ce831f34544c8fc716
Norman Saez <nfsaez@uc.cl>
2015-05-01 22:18:53 -0300

    Adding changes to improve this daemon

--- src/daemon/organize_imgs.py
+++ src/daemon/organize_imgs.py
@@ -8,16 +8,14 @@ import sys
 import argparse 
 
 parse = argparse.ArgumentParser()
-#parse.add_argument('-c', '--camera', dest='camera', type='str', help='Camera num: 0,1,2,3 etc', default="0")
-parse.add_argument('-p', '--prefix', dest='prefix', type=str, help='Camera prefix: default all', default="all")
-#parse.add_argument('-e', '--exptime', dest='exptime', type=int, help='Value for ExposureTimeAbs', default=1000)
 parse.add_argument('-d', '--directory', dest='directory', type=str, help='directory to be store the images', default=None)
-parse.add_argument('-t', '--adquisition_time', dest='adquisition_time', nargs='*', type=int, help='Image adquisition_time in seconds', default=[1])
 (options, unknown) = parse.parse_known_args()
-
 
 def move_files(filenames, dst):
     t0 = datetime.datetime.utcnow() 
+    filenames = glob.glob(src_dirname+'/*')
+    if not os.path.exists(dst_dirname) and len(filenames)>1:
+        os.makedirs(dst_dirname)
     for src in filenames:
         try:
             cmd = "mv %s %s" % (src, dst)
@@ -34,20 +32,19 @@ def signal_handler(signal, frame):
 
 if __name__ == '__main__':
 
-    src_dirname = '/home/lotuce2/lotuce2/src/acquisition/2015_04_23.13/'
+    src_dirname = options.directory
     dst_dirname = os.path.split(os.path.abspath(__file__))[0]
     filenames = []
     sts = mp.Queue()
     signal.signal(signal.SIGINT, signal_handler)
-    while(True):
+    count = 0
+    while(count <= 1):
         dst_dirname = dst_dirname+'/'+str(time.strftime("%Y_%m_%dT%H_%M", time.localtime()))
         dst_dirname = os.path.normpath(dst_dirname)
-        filenames = glob.glob(src_dirname+'/*')
-        if not os.path.exists(dst_dirname) and len(filenames)>1:
-            os.makedirs(dst_dirname)
+        filenames=None
         p = mp.Process(target=move_files, args=(filenames, dst_dirname))
         p.start()
         p.join()
         print sts.get().total_seconds()
         dst_dirname = os.path.split(os.path.abspath(__file__))[0]
-        
+        count += 1
