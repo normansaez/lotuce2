96933ea68ddb68c8a4c0171bc60a3394a00988de
Norman Saez <nfsaez@uc.cl>
2015-05-09 01:37:49 -0400

    increase refresh

--- src/gui/Calibra.py
+++ src/gui/Calibra.py
@@ -64,7 +64,7 @@ class Calibra(GObject.GObject):
     def __init__(self):
         GObject.GObject.__init__(self)
         self.counter = 0
-        GObject.timeout_add_seconds(5, self._cb_counter)
+        GObject.timeout_add_seconds(30, self._cb_counter)
         path, fil = os.path.split(os.path.abspath(os.path.realpath(__file__)))
         self.builder = Gtk.Builder()
         self.builder.add_from_file(path+"/glade/calibra.glade")
