97afbb2266fbcd775cd1f3d93d5a7165995c8265
Norman Saez <nfsaez@uc.cl>
2015-05-09 03:45:58 -0300

    Apply button enable

--- src/gui/Acquisition.py
+++ src/gui/Acquisition.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
-import time
 import darc
-import cairo
-import signal
 import ConfigParser
 
-from multiprocessing import Process
-from subprocess import Popen, PIPE
-from pylab import imshow,show
-from scipy import signal
-from matplotlib.figure import Figure
-from matplotlib.path import Path
-from darcaravis import DarcAravis
+from pylab import imshow
 
 from gi.repository import Gtk
-from gi.repository import Gdk
 from gi.repository import GObject
 
 import numpy as np
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-
 
 class Acquisition(GObject.GObject):
 
@@ -38,35 +26,26 @@ class Acquisition(GObject.GObject):
         self.window = self.builder.get_object ("window1")
         self.window.set_events(self.window.get_events())
         
-        self.DarcAravis = DarcAravis()
-
         self.configfile=path+'/../../conf/config.cfg'
         self.config = None
         self.config = ConfigParser.ConfigParser()
         self.config.read(self.configfile)
         
-        self.__step = "--"
+        self.__refresh = "--"
         if self.window:
             self.window.connect("destroy", Gtk.main_quit)
 
-        self.button_apply_subap = self.builder.get_object("apply_subap_button")
-        self.button_apply_refresh = self.builder.get_object("refresh_button")
-        self.button_apply_offset = self.builder.get_object("offset_step_button")
-
-        self.entry_subap = self.builder.get_object("subap_size_entry")
-        self.entry_refresh = self.builder.get_object("refresh_entry")
-        self.entry_offset = self.builder.get_object("offset_step_entry")
-
-        self.label_subap = self.builder.get_object("subap_label")
-        self.label_refresh = self.builder.get_object("refresh_label")
-        self.label_offset = self.builder.get_object("offset_step_label")
-################ fully test
+        self.button_apply_refresh = self.builder.get_object("button_apply")
+        self.entry_refresh = self.builder.get_object("entry_refresh")
+        self.label_refresh = self.builder.get_object("label_refresh")
+        #Camera IMG
         self.img_cam0 = self.builder.get_object("image_cam0")
         self.img_cam1 = self.builder.get_object("image_cam1")
         self.img_cam2 = self.builder.get_object("image_cam2")
         self.img_cam3 = self.builder.get_object("image_cam3")
         self.img_cov = self.builder.get_object("image_cov")
 
+        self.button_apply_refresh.connect("clicked", self._cb_refresh, "refresh")
         dic = { 
             "on_buttonQuit_clicked" : self.quit,
         }
@@ -234,65 +213,18 @@ class Acquisition(GObject.GObject):
         plt.savefig('covx.png')
         self.img_cov.set_from_file("covx.png")
 
-    def step_callback(self, widget, data=None):
+    def _cb_refresh(self, widget, data=None):
         '''
-        step_callback
-        '''
-        step = self.entry_offset.get_text()
-        print "step %s" % step
-        if step == "":
-            step = "10"
-        self.__step = int(step)
-        print "self.__step %d" % self.__step
-        self.label_offset.set_text("%s" % str(step))
-        self.entry_offset.set_text("")
-
-    def offset_callback(self, widget, data=None):
-        '''
-        offset callback
-
-        The offset is taking as reference darcplot gui.  Therefore the offset
-        cross follows that darcplot axis references.
-        '''
-#        print "offset: %s" % (data)
-#        offset_y = self.DarcAravis.get(self.camera, 'OffsetY')
-#        offset_x = self.DarcAravis.get(self.camera, 'OffsetX')
-#        print "before apply step(%d)\noffset(%d,%d)" % (self.__step, offset_x, offset_y)
-
-        if data == 'up':
-            val = offset_y + self.__step
-            print "to be applied: %d" % val
-            self.DarcAravis.set(self.camera, 'OffsetY', val)
-        if data == 'do':
-            val = offset_y - self.__step
-            print "to be applied: %d" % val
-            self.DarcAravis.set(self.camera, 'OffsetY', val)
-        if data == 'le':
-            val = offset_x + self.__step
-            print "to be applied: %d" % val
-            self.DarcAravis.set(self.camera, 'OffsetX', val)
-        if data == 'ri':
-            val = offset_x - self.__step
-            print "to be applied: %d" % val
-            self.DarcAravis.set(self.camera, 'OffsetX', val)
-
-        if data == 'first try':
-            x = self.DarcAravis.get(self.camera, 'Width')
-            y = self.DarcAravis.get(self.camera, 'Height')
-            offsetX = int((656 - x )/2.0)
-            offsetY = int((492 - y)/2.0)
-            self.DarcAravis.set(self.camera, 'OffsetX', offsetX)
-            self.DarcAravis.set(self.camera, 'OffsetY', offsetY)
-
-        offset_y = self.DarcAravis.get(self.camera, 'OffsetY')
-        offset_x = self.DarcAravis.get(self.camera, 'OffsetX')
-        if data =='up' or data =='do' or data == 'first try':
-            self.offset_y.set_text("%d pixel(s)"% int(offset_y))
-        if data =='le' or data =='ri' or data == 'first try':
-            self.offset_x.set_text("%d pixel(s)"% int(offset_x))
-        print "after apply: offset(%d,%d)" % (offset_x, offset_y)
         
-
+        '''
+        refresh = self.entry_refresh.get_text()
+        print "refresh every: %s" % refresh
+        if refresh == "":
+            refresh = "10"
+        self.__refresh = int(refresh)
+        print "self.__refresh %d" % self.__refresh
+        self.label_refresh.set_text("refresh every: %s" % str(refresh))
+        self.entry_refresh.set_text("")
 
     def _cb_counter(self):
         self.counter += 1
