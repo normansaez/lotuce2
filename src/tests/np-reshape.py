5cdb1ef048a0da580a107efc5727ced46d5ff714
Norman Saez <nfsaez@uc.cl>
2015-05-15 01:22:11 -0300

    profiles in python

--- src/tests/np-reshape.py
+++ src/tests/np-reshape.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python
 
-print "hola"
+import numpy as np
+
+if __name__ == '__main__':
+    pxly = 3
+    pxlx = 5
+
+    data = np.array(range(0,pxly*pxlx))
+    
+    index = 0
+    col = 0
+    px = 0
+
+    for i in range(0,pxlx):
+        py = 0
+        for j in range(0,pxly):
+            py = py + data[pxlx*j+i]
+            px = px + data[index]
+            col += 1
+            if col >= pxlx:
+                print "x>%d" % px
+                col = 0
+                px = 0
+            index += 1
+        print "  %d<y" % py
