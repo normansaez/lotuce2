bbaf1692c852023cab6611e7d9e2f8f74d86e1cb
Norman Saez <nfsaez@uc.cl>
2015-05-17 01:36:58 -0300

    Adding stuff

--- src/cpp-client/client.cpp
+++ src/cpp-client/client.cpp
@@ -1,5 +1,5 @@
 #include <control.hh>
-#include <controlSK.cc>
+//#include <controlSK.cc>
 #include <darc.h>
 //#include <stdio.h>
 //#include <stdlib.h>
@@ -10,40 +10,9 @@ int main(int argc, char** argv)
 {
 
     try {
-        //    // Initialise the ORB.
         CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
-        //
-        //    // Obtain a reference to the root POA.
         CORBA::Object_var obj = orb->resolve_initial_references("RootPOA");
-        PortableServer::POA_var poa = PortableServer::POA::_narrow(obj);
-        //
-        //      // We allocate the object on the heap.  Since this is a reference
-        //      // counted object, it will be deleted by the POA when it is no
-        //      // longer needed.
-        //    Echo_i* myecho = new Echo_i();
-        //
-        //    // Activate the object.  This tells the POA that this object is
-        //    // ready to accept requests.
-        //    PortableServer::ObjectId_var myechoid = poa->activate_object(myecho);
-        //
-        //      // Obtain a reference to the object.
-        //    Echo_var myechoref = myecho->_this();
-        //
-        //    // Decrement the reference count of the object implementation, so
-        //    // that it will be properly cleaned up when the POA has determined
-        //    // that it is no longer needed.
-        //    myecho->_remove_ref();
-        //
-        //    // Obtain a POAManager, and tell the POA to start accepting
-        //    // requests on its objects.
-        //    PortableServer::POAManager_var pman = poa->the_POAManager();
-        //    pman->activate();
-        //
-        //    // Do the client-side call.
-        //    hello(myechoref);
-        //
-        //    // Clean up all the resources.
-        //    orb->destroy();
+        RTC::Control_var controlref = RTC::Control::_narrow(obj);
     }
     catch(CORBA::COMM_FAILURE& ex) {
         cerr << "Caught system exception COMM_FAILURE -- unable to contact the "
