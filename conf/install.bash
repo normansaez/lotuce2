9e55d50c531a80cce13f63fc587890d491273c7b
Norman Saez <nfsaez@uc.cl>
2015-12-08 16:18:15 -0300

    Adding ram img

--- conf/install.bash
+++ conf/install.bash
@@ -1,2 +1,3 @@
 cp -v ${HOME}/lotuce2/conf/dhcpd.conf /etc/
 cp -v ${HOME}/lotuce2/conf/dhclient.conf /etc/dhcp/dhclient.conf 
+mkdir -p /mnt/ramcache && mount -t tmpfs -o size=4g none /mnt/ramcache/
