0489322cc56eafc59cdd79c5277e24c3a5ad40a3
Norman Saez <nfsaez@uc.cl>
2015-12-08 16:19:12 -0300

    Adding installation

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='DarcAravis',
+      version='1.0',
+      description='Python Darc Aravis Utilities',
+      author='Norman Saez',
+      author_email='nfsaez@uc.cl',
+      url='https://github.com/normansaez/lotuce2.git',
+      packages=['darcaravis'],
+     )
