26983be64e331202def003c0385884dc56ed4752
Norman Saez <nfsaez@uc.cl>
2016-03-02 10:21:58 -0300

    fixing path

--- src/setup/set_frequency.py
+++ src/setup/set_frequency.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     if hz.isdigit():
         cmd = """ssh ubuntu@192.168.0.203 'sudo date -s "%s"'""" % now.strftime("%d %b %Y %H:%M:%S")
         os.system(cmd)
-        cmd =   'ssh ubuntu@192.168.0.203 "sudo python /home/ubuntu/bbb-pru-clock/blinkled.py %s"' % hz
+        cmd =   'ssh ubuntu@192.168.0.203 "sudo python /home/ubuntu/lotuce2/src/beagleclock/beagleclock.py --hertz %s"' % hz
         os.system(cmd)
