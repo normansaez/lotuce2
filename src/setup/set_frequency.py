69a6bcf97b863e62e1928fe905d7a6460355fc4f
Norman Saez <nfsaez@uc.cl>
2016-03-20 16:04:13 +0000

    change acording distro

--- src/setup/set_frequency.py
+++ src/setup/set_frequency.py
@@ -2,12 +2,26 @@
 import sys
 import os
 import datetime
+import platform
+
 if __name__ == '__main__':
     a = datetime.datetime
     now = a.now()
     hz = sys.argv[1]
     if hz.isdigit():
-        cmd = """ssh ubuntu@192.168.0.203 'sudo date -s "%s"'""" % now.strftime("%d %b %Y %H:%M:%S")
-        os.system(cmd)
-        cmd =   'ssh ubuntu@192.168.0.203 "sudo python /home/ubuntu/lotuce2/src/beagleclock/beagleclock.py --hertz %s"' % hz
-        os.system(cmd)
+        user = "ubuntu"
+        ipadd = "192.168.0.203"
+        home = "/home/%s" % user
+
+        if platform.dist()[0] == 'debian':
+            user = "root"
+            ipadd = "192.168.0.16"
+            home ="/%s" % user
+
+        cmd1 = """ssh %s@%s 'sudo date -s "%s"'""" % (user, ipadd, now.strftime("%d %b %Y %H:%M:%S"))
+        cmd2 =   'ssh %s@%s "sudo python %s/lotuce2/src/beagleclock/beagleclock.py --hertz %s"' % (user, ipadd, home, hz)
+#        print cmd1
+#        print cmd2
+        os.system(cmd1)
+        os.system(cmd2)
+
